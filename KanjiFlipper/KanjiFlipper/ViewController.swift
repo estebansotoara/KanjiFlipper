589c9b0bcac5057ff866b8fac33412cdf184459d
Arae <esteban.soto.dev@gmail.com>
2020-10-03 00:34:57 -0700

    First commit

---
+++ KanjiFlipper/KanjiFlipper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanjiFlipper
+//
+//  Created by AraDev on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
